b846e2bffd2d0f1cd5f0a8e0df4ec4f460a653c3
Vivien Galuchot <vivien.galuchot@gmail.com>
2015-04-10 21:59:25 +0200

    restait du caca

--- moduleGPS.c
+++ moduleGPS.c
@@ -8,18 +8,5 @@ void initGPS(){
 }
 
 void traiterDataGPS(){
-	if(iBuff1>200){
-		selBuff = 0;
-		bufferUART1[iBuff1] = 0;
-		//debug_printf("--buff1--");
-		debug_printf(bufferUART1);
-		iBuff1 = 0;
-	}
-	else if(iBuff0>200){
-		selBuff = 1;
-		bufferUART0[iBuff0] = 0;
-		//debug_printf("--buff0--");
-		debug_printf(bufferUART0);
-		iBuff0 = 0;
-	}
+	//
 }
