4a7703401b3c6eff5436bb71e0e5a11aea29b231
Syrok <syrok.vincent@hotmail.fr>
2015-04-13 17:43:22 +0200

    Maj ecran

--- ecran.c
+++ ecran.c
@@ -1,19 +1,81 @@
 #include <ecran.h>
 #include <uart.h>
 
-void printe( char *phrase,int ligne,int colonne)
+void printe( char *phrase,char ligne,char colonne,char *BGcolor, char *FGcolor)
 {
+	moveCursor(ligne,colonne);
+	setTextBGColour(BGcolor);
+	setTextFGColour(FGcolor);
+	putstr(phrase);
 
+}
 
-	sendCharTableTX1(MoveCursor,3);
-	sendCharTX1((char)ligne);
+void moveCursor(char ligne, char colonne){
+
+	sendCharTX1(0xff);
+	sendCharTX1(0xe4);
 	sendCharTX1(0x00);
-	sendCharTX1((char)colonne);
+	sendCharTX1(ligne);
+	sendCharTX1(0x00);
+	sendCharTX1(colonne);
 	waitACK_RX_1();
+}
 
+void setTextFGColour(char *couleur){
+	sendCharTX1(0xff);
+	sendCharTX1(0x7f);
+	sendCharTableTX1(couleur,2);
+	waitACK_RX_1();
+}
 
+void setTextBGColour(char *couleur){
+	sendCharTX1(0xff);
+	sendCharTX1(0x7e);
+	sendCharTableTX1(couleur,2);
+	waitACK_RX_1();
+}
+
+void putstr(char *phrase){
+
+	sendCharTX1(0x00);
 	sendCharTX1(0x06);
 	sendStrTX1(phrase);
 	sendCharTX1(0x00);
 	waitACK_RX_1();
 }
+
+void drawRectangle(char x1,char y1,char x2, char y2, char *couleur){
+
+	sendCharTX1(0xff);
+	sendCharTX1(0xcf);
+	sendCharTX1(0x00);
+	sendCharTX1(x1);
+	sendCharTX1(0x00);
+	sendCharTX1(x2);
+	sendCharTX1(0x00);
+	sendCharTX1(y1);
+	sendCharTX1(0x00);
+	sendCharTX1(y2);
+	sendCharTableTX1(couleur,2);
+	waitACK_RX_1();
+
+}
+
+void drawFilledRectangle(char x1,char y1,char x2, char y2, char *couleur){
+
+	sendCharTX1(0xff);
+	sendCharTX1(0xce);
+	sendCharTX1(0x00);
+	sendCharTX1(x1);
+	sendCharTX1(0x00);
+	sendCharTX1(x2);
+	sendCharTX1(0x00);
+	sendCharTX1(y1);
+	sendCharTX1(0x00);
+	sendCharTX1(y2);
+	sendCharTableTX1(couleur,2);
+	waitACK_RX_1();
+
+}
+
+
--- ecran.h
+++ ecran.h
@@ -6,17 +6,37 @@
 #include <uart.h>
 #include <ports.h>
 
-static char MoveCursor[] = {0xFF,0xe4,0x00};
+
+
 static char clear[] = {0xFF,0xD7};
 static char landscapeR[] = {0xFF,0x68, 0x00,0x01};
 static char fondBleu[] = {0xFF,0xCE, 0x00,0x00, 0x00,0x00, 0x00,0x80, 0x00,0x80, 0x00,0x4D};
 static char rondBleuClair[] = {0xFF,0xCC, 0x00,0x40, 0x00,0x40, 0x00,0x32, 0x04,0xB9};
-static char txtHeight[] = {0xFF,0x7B, 0x00,0x03};
-static char txtWidth[] = {0xFF,0x7C, 0x00,0x03};
+static char txtHeight[] = {0xFF,0x7B, 0x00,0x02};
+static char txtWidth[] = {0xFF,0x7C, 0x00,0x02};
 static char putStr[] = {0x00,0x06,'G','P','S',0x00};
 
+static char blanc[]={0xff,0xff};
+static char fondviolet[]={0xd0,0xcf};
+static char tourviolet[]={0xeb,0xd6};
+static char fondbleu[]={0x19,0x71};
+static char tourbleu[]={0x1e,0xde};
+static char fondorange[]={0xf3,0xa0};
+static char tourorange[]={0xf7,0xa0};
+static char fondgris[]={0x6b,0x6d};
+static char tourgris[]={0xc6,0x18};
 
-void printe( char *phrase,int ligne,int colonne);
+
+
+
+
+void printe( char *phrase,char ligne,char colonne,char *BGcolor, char *FGcolor);
+void moveCursor(char ligne,char colonne);
+void setTextFGColour(char *couleur);
+void setTextBGColour(char *couleur);
+void putstr(char *phrase);
+void drawRectangle(char x1,char y1,char x2, char y2, char *couleur);
+void drawFilledRectangle(char x1,char y1,char x2, char y2, char *couleur);
 
 
 
